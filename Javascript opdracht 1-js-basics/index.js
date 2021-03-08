9407ca5138753d59c5937e0fb59f6fbee5d407a7
JosePluymaekers <79280564+JosePluymaekers@users.noreply.github.com>
2021-03-08 22:42:19 +0100

    Add files via upload

---
+++ Javascript opdracht 1-js-basics/index.js
@@ -0,0 +1,20 @@
+// this is my first javascript code!
+
+console.log("Hello Winc Academy")
+
+let name = 'jose';
+console.log(name);
+
+let plusSom = 4 + 4;
+console.log(plusSom)
+
+let minSom = 12 - 6;
+console.log(minSom)
+
+let keer = 3 * 3;
+console.log(keer)
+
+let delen = 120 / 10;
+console.log(delen)
+
+let leeftijd = 'vierendertig';
